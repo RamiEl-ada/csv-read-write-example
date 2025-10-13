5426adf5d7ee981954d879054eb3f65a19d4ab7c
Rami-Ada <rami.elberdai-nasihi@ada.ac.uk>
2025-10-13 11:35:07 +0000

    student tracker tasks completed

--- 1_student_score_tracker.py
+++ 1_student_score_tracker.py
@@ -1,6 +1,8 @@
 # Reading CSV files - Student Scores Tracker
 with open('1_students.csv', 'r', encoding='utf-8') as file:
-    line_number = 0
+    line_number = 0 
+    highest_average = 0
+    lowest_average = 100
     for line in file:
         row = line.strip().split(',')
         if line_number == 0:
@@ -9,8 +11,17 @@ with open('1_students.csv', 'r', encoding='utf-8') as file:
             name = row[0]
             scores = [int(score) for score in row[1:]]
             average = sum(scores) / len(scores)
+            if average < lowest_average:
+                lowest_average = average
+                lowest_holder_name = name
+            elif highest_average < average:
+                highest_average = average
+                highest_holder_name = name
             print(f"{name} - Scores: {scores} | Average: {average:.2f}")
-        line_number += 1
+        line_number+=1 
+
+print(f"Highest Average: {highest_average:.2f}, Lowest Average: {lowest_average:.2f},")
+print(f"Highest Average Holder: {highest_holder_name}\nLowest Average Holder: {lowest_holder_name}")
 
 # Extension task: Modify the program to print the highest and lowest average scores among all students 
 # along with the names of the students who acheived these scores.
